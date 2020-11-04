03d3549eda7f482dd26399f120d6fecbda75ccb7
Ben S <233921@shakopeeschools.org>
2020-11-04 14:16:08 -0600

    Update 2 Team01
    Added strategy

--- team0.py
+++ team0.py
@@ -25,6 +25,15 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
+    if len(my_history)==0:
+        return 'c'
+    elif their_history[-1]=='c':
+        return my_history[-1]
+    elif their_history[-1]=='b':
+        if my_history[-1] == 'c':
+            return 'b'
+        else:
+            return 'c'
     
     return 'c'
 
