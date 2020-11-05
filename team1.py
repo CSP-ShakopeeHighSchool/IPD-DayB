f38c1493ad9b8cb1e86c5c5b59c70dfe335e051f
Ben S <233921@shakopeeschools.org>
2020-11-05 13:22:55 -0600

    Fix
    Accidentally wrote in team 0 not team 1

--- team1.py
+++ team1.py
@@ -26,7 +26,20 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
 
-    return 'c'
+    if len(my_history)==0:
+        return 'c'
+    
+    elif their_history[-1]=='c':
+        return my_history[-1]
+
+    elif their_history[-1]=='b':
+        if my_history[-1] == 'c':
+            return 'b'
+        else:
+            return 'c'
+    
+    else:
+        return 'c'
 
 
 def test_move(my_history, their_history, my_score, their_score, result):
